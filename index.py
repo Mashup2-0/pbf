f47fb0f5c0de05ec76f28e1763e393a6a455c064
rotem <rotemzzz@gmail.com>
2020-01-17 14:01:30 +0200

    get feature vector

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 import numpy as np
+import torch
 from PIL import Image
+from models import IR_50
 
 from align.align_trans import warp_and_crop_face, get_reference_facial_points
 from align.detector import detect_faces
@@ -12,12 +14,18 @@ crop_size = 112  # specify size of aligned faces, align and crop with padding
 scale = crop_size / 112.
 reference = get_reference_facial_points(default_square=True) * scale
 
+model = IR_50((112, 112)).eval()
+model.load_state_dict(torch.load('backbone_ir50_ms1m_epoch120.pth'))
+
 for landmark in landmarks:
     facial5points = [[landmark[j], landmark[j + 5]] for j in range(5)]
-    warped_face = warp_and_crop_face(np.array(img), facial5points, reference, crop_size=(96, 112))
-    img_warped = Image.fromarray(warped_face)
+    warped_face = warp_and_crop_face(np.array(img), facial5points, reference, crop_size=(112, 112))
+    img_warped = torch.Tensor(warped_face).unsqueeze(0).transpose(1, 3)
+    out = model(img_warped).detach().cpu().numpy()
+    norm = np.linalg.norm(out, axis=1)
+    out = out / norm
+    i = 5
 
-    img_warped.show()
 
 
 
